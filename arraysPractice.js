1613cd8e36db786c1969167603460fbcd8ded49e
Mike <mike.chi4@gmail.com>
2016-09-19 16:49:31 -0600

    finished up to divider

--- arraysPractice.js
+++ arraysPractice.js
@@ -5,7 +5,9 @@ var arr = [10,20,30];
 //Create a function named 'first' that is given 'arr' as the argument and  returns the first item in the given array.
 
   //Code Here
-
+function first(arr){
+  return arr[0];
+}
 
 //Next problem
 
@@ -16,7 +18,9 @@ var arr = [40,50,60];
 
 
   //Code Here
-
+function last(arr){
+  return arr[arr.length - 1];
+}
 
 //Next Problem
 
@@ -25,7 +29,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 //Create a function named 'looper' that is given family as it's only argument, loops through the given array, and alerts every item in the array.
 
   //Code Here
-
+function looper(arr){
+  for(var i = 0; i < arr.length; i++) {
+    alert(arr[i]);
+  }
+}
 
 //Next problem
 
@@ -35,7 +43,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 //Write a function called reversedLooper that is given letters as it's only argument and loops through the given array backwards alerting every item in the array starting at the end.
 
   //Code Here
-
+function reversedLooper(arr) {
+  for(var i = arr.length - 1; i >=0; i--) {
+    alert(arr[i]);
+  }
+}
 
 //Next Problem
 
@@ -44,7 +56,14 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 //Write a function named evenFinder that is given nums as it's only argument and removes all values that aren't even from the given array.
 
   //Code Here
-
+function evenFinder(arr) {
+  for(var i = 0; i < arr.length; i++) {
+    if(arr[i] % 2 !== 0) {
+      arr.splice(i,1);
+    }
+  }
+  return arr;
+}
 
 //Next problem
 
@@ -53,6 +72,21 @@ var numbersArray = [1,2,34,54,55,34,32,11,19,17,54,66,13];
 //Write a function called divider that is given one argument, numbersArray.
 //Have divider return an Array with the first item in the array being the evens array (all the even values from nums) and the second item in the Array being the odds array(all the odd values from nums).
 
+function divider(arr) {
+  var evens =[];
+  var odds =[];
+  var result = [];
+  for(var i = 0; i < arr.length; i++) {
+    if(arr[i] % 2 === 0) {
+      evens.push(arr[i]);
+    } else {
+      odds.push(arr[i]);
+    }
+  }
+  result.unshift(evens);
+  result.push(odds);
+  return result;
+}
 
 
   //Code Here
